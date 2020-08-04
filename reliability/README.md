28b96d7a8cd1c0351d69e13e5a876ad7aba8b2ed
angela <angela@matic.network>
2020-08-04 17:00:25 +0530

    add bidirectional lstm model

--- reliability/README.md
+++ reliability/README.md
@@ -1,12 +1,12 @@
 ## @
 
-http://3.19.215.44/reliability
+`@/reliability`
 
 ## Files
 
-1. `news-app.py`
-2. `requirements.txt`
-3. `RealiabilityAnalysis.ipynb`
-4. `training_linear.py`
-5. `tfidf_vectorizer.pk`
-6. `linear_clf_model.pk`
+1. `reliability-app.py`: deployed streamlit app. Input content, output reliability score.
+2. `notebooks/RealiabilityAnalysis.ipynb`: linear naive bayes(multinomialNB) model trained on a dataset of ~5200 entries
+3. `notebooks/Realiabilityv2.ipynb`: bidirectional LSTM model trained on ~20,500 entries
+4. `content/saved_model`: LSTM model h5 file
+5. `content/tokenizer.pk`: tokenizer pickle
+6. `requirements.txt`
---
+++ reliability/reliability-app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+
+import tensorflow as tf
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+max_features=4500
+
+model = tf.keras.models.load_model(
+  'content/saved_model', custom_objects=None, compile=True, options=None
+)
+tokenizer = pickle.load(open(
+  'content/tokenizer.pk', 'rb'
+))
+
+st.title('News: Reliability')
+
+# analyse
+# input -> news article
+def analyse (txt):
+  transformed_news = tokenizer.texts_to_sequences([txt])
+  padded = pad_sequences(transformed_news, maxlen=max_features, padding='post')
+  pred_prob = model.predict(padded)
+  pred_class = model.predict_classes(padded)
+  prob = [prob*100 for prob in pred_prob]
+
+  st.write(pd.DataFrame({
+       'class 0 (Reliable) %': [prob[0][0]],
+       'class 1 (Unreliable) %': [prob[0][1]],
+   }))
+
+  if (pred_class[0] == 0):
+    st.write('Predicted class:', pred_class[0], '/ Reliable')
+  elif (pred_class[0] == 1):
+    st.write('Predicted class:', pred_class[0], '/ Unreliable')
+
+txt = st.text_area (
+  label='Text to analyze', 
+  value="Enter content here ...",
+  height=24
+)
+
+if st.button('Check'):
+  analyse(txt)
